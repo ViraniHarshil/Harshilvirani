61b66df26e9e642af8a37c2ad7f793c615b6a027
Harshilvirani <harshilvirani4899@gmail.com>
2023-06-21 19:51:58 +0530

    ch1

---
+++ ch-1/3.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+	float r;
+	clrscr();
+
+	printf("value of raduios:");
+	scanf ("%f",&r);
+	printf("area of circle : %f\n\n", 3.14 * r * r);
+
+	getch();
+}
+
+
+
---
+++ ch-1/H.C
@@ -0,0 +1,15 @@
+# include<stdio.h>
+# include<conio.h>
+main()
+{
+	int a=10;
+	int b=5;
+	clrscr();
+
+	printf("%d + %d = %d\n\n",a, b, a+b);
+	printf("%d - %d = %d\n\n",a, b, a-b);
+	printf("%d * %d = %d\n\n",a, b, a*b);
+
+	getch();
+}
+
